d5fdb7d82b22526e61b578727118cefc46bc2a6a
Zhao Shu <zhaobenben007@googlemail.com>
2020-10-01 13:06:38 +0200

    trim the function parameters

--- assignment-1/code/eclipse_bug_prediction.R
+++ assignment-1/code/eclipse_bug_prediction.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # calculate the impurity based on Gini index for binary classifications
 # y = one column of the label - multiple rows = vector
-impurity_by_gini_index <- function(y) {
+impurity <- function(y) {
   
   zero.percent <- length(y[y == 0]) / length(y)
   one.percent <- 1 - zero.percent
@@ -19,31 +19,31 @@ bestsplit <- function(x, y) {
   x.sorted <- sort(unique(x))
   x.sorted.length <- length(x.sorted)
   
+  impurity.parent <- impurity(y)
+  # initialize the best split point and its reduction of impurity.
+  # the reduction of impurity is supposed to be the maximum reduction of impurity.
+  impurity.reduction.max <- 0
+  x.splitpoints.best <- NA
+  
   # if there is only one unique value in x
   # the maximum impurity reduction is 0
   # the best split point is NA
   if(x.sorted.length < 2) {
-    return(c(0, NA))
+    return(c(impurity.reduction.max, x.splitpoints.best))
   }
   
   # e.g. x = c(1, 2, 3, 4, 5): x has 5 values, and thus x has (5 - 1) = 4 split points.
   # x.splitpoints = (c(1, 2, 3, 4) + c(2, 3, 4, 5)) / 2
   x.splitpoints <- (x.sorted[1:x.sorted.length-1] + x.sorted[2:x.sorted.length]) / 2
   
-  impurity.parent <- impurity_by_gini_index(y)
-  # initialize the best split point and its reduction of impurity.
-  # the reduction of impurity is supposed to be the maximum reduction of impurity.
-  impurity.reduction.max <- 0
-  x.splitpoints.best <- NULL
-  
   for(x.splitpoint in x.splitpoints) {
     # the left children of a parent: x is less than and equal to the value of the split point.
     proportion.left.children <- length(y[x <= x.splitpoint]) / length(y)
-    impurity.left.children <- impurity_by_gini_index(y[x <= x.splitpoint])
+    impurity.left.children <- impurity(y[x <= x.splitpoint])
     
     # the right children of a parent: x is greater than the value of the split point.
     proportion.right.children <- length(y[x > x.splitpoint]) / length(y)
-    impurity.right.children <- impurity_by_gini_index(y[x > x.splitpoint])
+    impurity.right.children <- impurity(y[x > x.splitpoint])
     
     # calculate the reduction of impurity
     impurity.reduction <- impurity.parent - (proportion.left.children*impurity.left.children + proportion.right.children*impurity.right.children)
@@ -74,7 +74,7 @@ vote_of_majority <- function(y) {
 # y = one column of the label - multiple rows = vector
 # nmin = the minimum number of observations that a node must contain
 # minleaf = the minimum number of observations required for a leaf node
-# nfeat = the set of predictors, e.g. nfeat = c(1, 2, 3, 4, 5) or nfeat = c(1, 2, 4)
+# nfeat = the number of predictors used to grow the tree
 tree_grow <- function(x, y, nmin, minleaf, nfeat) {
   
   # nodelist <- {{training data}}
@@ -89,6 +89,9 @@ tree_grow <- function(x, y, nmin, minleaf, nfeat) {
   # initialize the model of the trained tree
   tree <- list()
   tree.node.index <- 0
+  
+  # nfeat.set is sampled out of all the predictors from [1, ncol(x)] to choose the number of nfeat predictors
+  nfeat.set <- sort(sample(ncol(x), nfeat))
   
   repeat{
     
@@ -105,7 +108,7 @@ tree_grow <- function(x, y, nmin, minleaf, nfeat) {
     nodelist[[1]] <- NULL
     
     # if impurity(current node) > 0 AND observations(current node) >= nmin, the current node is allowed to be split.
-    if(impurity_by_gini_index(current.node.y) > 0 && length(current.node.y) >= nmin) {
+    if(impurity(current.node.y) > 0 && length(current.node.y) >= nmin) {
       
       # Find one best feature to split the data
       # Feature is represented by its index in the table
@@ -117,7 +120,7 @@ tree_grow <- function(x, y, nmin, minleaf, nfeat) {
       # find the best split point of the best split feature with the maximum impurity reduction.
       # S <- set of candidate splits in current node
       # s* <- argmax {impurity reduction(s, current node)}, where s is in S
-      for(feat in nfeat) {
+      for(feat in nfeat.set) {
         result <- bestsplit(current.node.x[,feat], current.node.y)
         
         # if there does not exist the best split for the current feature
@@ -167,7 +170,7 @@ tree_grow <- function(x, y, nmin, minleaf, nfeat) {
       
     } else {
       
-      if (impurity_by_gini_index(current.node.y) == 0) {
+      if (impurity(current.node.y) == 0) {
         # if the current node is pure:
         # append the current node as the terminal node to the tree model by its pure label.
         tree.node.index <- tree.node.index + 1
@@ -239,9 +242,7 @@ tree_pred <- function(x, tr) {
 
 # grow the classification tree by the bagging with random forest
 # m = the number of bootstrap samples to be drawn
-# sample_size = the size of one bootstrap sample (training set) < the size of table
-# nfeat_subset_size = the size of the subset of nfeat < the total number of predictors
-tree_grow_b <- function(x, y, nmin, minleaf, nfeat, m, sample.size, nfeat.subset.size) {
+tree_grow_b <- function(x, y, nmin, minleaf, nfeat, m) {
   # a list of m different trees
   trees <- list()
   tree.index <- 0
@@ -249,14 +250,10 @@ tree_grow_b <- function(x, y, nmin, minleaf, nfeat, m, sample.size, nfeat.subset
   # draw m samples with replacement from "table"
   for(i in 1:m) {
     # draw a sample (training set) with replacement from "table"
-    indices <- sample(length(y), sample.size, replace = TRUE)
-    
-    # random forest: randomly select a subset of nfeat
-    # 1 <= nfeat_subset_size < nfeat
-    nfeat.subset <- sample(nfeat, nfeat.subset.size)
+    indices <- sample(length(y), round(length(y)*0.9), replace = TRUE)
     
     # grow a tree on this sample
-    tr <- tree_grow(x[indices,], y[indices], nmin, minleaf, nfeat.subset)
+    tr <- tree_grow(x[indices,], y[indices], nmin, minleaf, nfeat)
     
     # append this tree to the list of trees
     tree.index <- tree.index + 1
@@ -270,30 +267,30 @@ tree_grow_b <- function(x, y, nmin, minleaf, nfeat, m, sample.size, nfeat.subset
 # predict the class label based on the voting of all the trees returned by tree_grow_b()
 # x = all the columns of the numeric predictors - one row = vector
 # trees = the list of the models of the trees
-tree_pred_b_by_one_row <- function(x, trees) {
+tree_pred_b_by_one_row <- function(x, trs) {
   # predictions of all the trees
-  preds.trees <- c()
+  preds <- c()
   
   # iterate through all the trees to calculate the predictions
-  for(tr in trees) {
+  for(tr in trs) {
     pred <- tree_pred(x, tr)
-    preds.trees <- c(preds.trees, pred)
+    preds <- c(preds, pred)
   }
   
   # return the majority vote of all the trees
-  vote_of_majority(preds.trees)
+  vote_of_majority(preds)
 }
 
 # predict the class label based on the voting of all the trees returned by tree_grow_b()
 # x = all the columns of the numeric predictors - multiple rows = matrix or vector
 # trees = the list of the models of the trees
-tree_pred_b <- function(x, trees) {
+tree_pred_b <- function(x, trs) {
   
   if(is.null(nrow(x))) {
     # if x contains only one row
-    label <- tree_pred_b_by_one_row(x, trees)
+    pred <- tree_pred_b_by_one_row(x, trs)
     # return the prediction
-    label
+    pred
   } else {
     # if x contains more than one rows
     # predictions of all the rows from x
@@ -303,9 +300,9 @@ tree_pred_b <- function(x, trees) {
       # pick one row from x
       x.row <- x[x.row.index,]
       
-      label <- tree_pred_b_by_one_row(x.row, trees)
+      pred <- tree_pred_b_by_one_row(x.row, trs)
       # add the prediction to the predictions
-      preds <- c(preds, label)
+      preds <- c(preds, pred)
     }
     # return all the predictions
     preds
@@ -363,7 +360,7 @@ y.test[y.test!=0] = 1
 # train the single classification tree on the training set
 nmin <- 15
 minleaf <- 5
-nfeat <- c(1:41)
+nfeat <- 41
 tr <- tree_grow(x.train, y.train, nmin, minleaf, nfeat)
 
 # predict based on the single classification tree on the test set
@@ -375,9 +372,7 @@ result
 
 # train the classification tree by bagging on the training set
 m <- 100
-sample.size <- 100
-nfeat.subset.size <- 41
-tr.bagging <- tree_grow_b(x.train, y.train, nmin, minleaf, nfeat, m, sample.size, nfeat.subset.size)
+tr.bagging <- tree_grow_b(x.train, y.train, nmin, minleaf, nfeat, m)
 
 # predict based on the classification tree by bagging on the test set
 tr.bagging.preds <- tree_pred_b(x.test, tr.bagging)
@@ -387,8 +382,8 @@ result <- statistics(y.test, tr.bagging.preds)
 result
 
 # train the classification tree by random forest on the training set
-nfeat.subset.size <- 6
-tr.random.forest <- tree_grow_b(x.train, y.train, nmin, minleaf, nfeat, m, sample.size, nfeat.subset.size)
+nfeat <- 6
+tr.random.forest <- tree_grow_b(x.train, y.train, nmin, minleaf, nfeat, m)
 
 # predict based on the classification tree by random forest on the test set
 tr.random.forest.preds <- tree_pred_b(x.test, tr.random.forest)
